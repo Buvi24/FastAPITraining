ac76bdd50d7ed6de7683dcd87050fa474bd22651
Buvi24 <2025cs_buvi_b@nie.ac.in>
2026-09-22 13:29:23 +0530

    Path parameters with type hint

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -18,4 +18,10 @@ def create_something():
 @app.get("/student/{usn}")
 def get_result(usn):
     return {"Result":"Distinction","USN":usn}
+#Path parameters with type hint
+@app.get("/candidate/{roll}")
+def get_candidate(roll:int):
+    return {"Result":"Distinction","Roll no":"roll","type":str(type(roll))}
+
+
            
